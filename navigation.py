93f9a8897065cd268820dc08d1e2c026265b3857
Dustin Stansbury <dustin.stansbury@gmail.com>
2024-02-22 12:15:30 -0800

    Add toc generator; wide mode by default

--- navigation.py
+++ navigation.py
@@ -1,3 +1,4 @@
+import string
 import streamlit as st
 from streamlit.components.v1 import html
 from config import SHOW_BUY_ME_A_BEER
@@ -14,10 +15,69 @@ change_link_color = """
 """
 
 
+def make_toc(sections):
+    """Janky generator for adding table-of-contents to the sidebar. Supports
+    one level of section nesting.
+
+    Parameters
+    ----------
+    sections : List[str]
+        A list of actual section names on the page to include in the TOC.
+        If a section names starts with "-", then we nest it under the previous
+        section.
+    """
+    toc_markdown = ""
+    subsection_idx = 0
+    subsection_name = list(string.ascii_lowercase)
+    n_sections = len(sections)
+    for ii, section in enumerate(sections):
+        prefix = ""
+        postifx = "\\" if ii < n_sections - 1 else ""
+
+        if section.startswith("-"):
+            section = section.replace("-", "").strip()
+            prefix_name = subsection_name[subsection_idx]
+            subsection_idx += 1
+            prefix = f"{prefix_name}. "
+        else:
+            subsection_idx = 0
+        section_link = (
+            section.lower().replace("(", "").replace(")", "").replace(" ", "-")
+        )
+        toc_markdown += f"\n{prefix}[{section}](#{section_link}) {postifx}"
+
+    st.sidebar.markdown(toc_markdown)
+
+    # Style the links
+    change_toc_link_color = """
+    <style>
+        a:visited{
+            color:None;
+            background-color: transparent;
+            text-decoration: none;
+        }
+        a:hover{
+            color:red;
+            background-color: transparent;
+            text-decoration: none;
+        }
+
+        a:link {
+            color: None;
+            background-color: transparent;
+            text-decoration: none;
+    }
+    </style>
+    """
+    st.sidebar.markdown(change_toc_link_color, True)
+
+
 def make_sidebar():
     """
     Manually create nav/TOC sidebar (default nav sidebar turned off in config.toml)
     """
+    st.set_page_config(layout="wide")
+
     with st.sidebar:
         st.markdown(change_link_color, unsafe_allow_html=True)
         st.title("Causal Inference for Data Science")
